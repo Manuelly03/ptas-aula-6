4e53db2b37558b57f09884addc3a6649baaddad6
Manuelly03 <fernandesmanuelly341@gmail.com>
2025-10-15 17:30:29 -0400

    fisrt commit

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import express from "express"
 const app = express()
+app.use(express.json())
 
+usuarios = []
 
 app.get('/', (req, res) => {
-    res.send("Voce está na pagina principal")
+    res.status(200).json(usuarios)
 
   } )
-  app.post('/', (req, res) => {
-    res.send("Voce está na pagina principal usando post")
+
+  app.post('/usuario', (req, res) => {
+    const {nome,idade} = req.body
+    console.log(nome, idade)
+    usuarios.push({nome,idade})
+    console.log(usuarios)
+
+    res.status(201).send("Usuario Criado")
   })
+
   
 app.listen(3333, () => {
     console.log("Servidor rodando na porta em http://localhost:3333")
